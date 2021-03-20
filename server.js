711148600ad495d378b4d8cecc30b28eedc1c47a
ron erlih <ronerlih@gmail.com>
2021-03-20 14:54:53 -0400

    change to hostname

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.post("/upload", function (req, res) {
 	var tmpName = req.files.img.path.substring(indx + 1);
    
    
-	var urlParmas = "?image_url=https://" + req.host + "/img/" + tmpName + "&btnG=Search+by+image&encoded_image=&image_content=&filename=&hl=en";
+	var urlParmas = "?image_url=https://" + req.hostname + "/img/" + tmpName + "&btnG=Search+by+image&encoded_image=&image_content=&filename=&hl=en";
 	// "?image_url=https://" + req.host + ":8080/img/" + tmpName + "&btnG=Search+by+image&encoded_image=&image_content=&filename=&hl=en";
    
    console.log({urlParmas});
