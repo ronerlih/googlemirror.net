0a3ee038d17cc54682e221242bb38c372cbdf0d0
ron erlih <ronerlih@gmail.com>
2021-03-21 18:33:59 -0400

    log to stdout

--- server.js
+++ server.js
@@ -61,7 +61,6 @@ app.post("/upload", function (req, res) {
 	axios
 		.get("https://www.google.com/searchbyimage/upload" + urlParmas, {
 			headers: {
-				"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
 			},
 		})
 		.then((result) => {
