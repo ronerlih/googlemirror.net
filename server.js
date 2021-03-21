1c1fb6d425fa100aef595e1463b8eee2844b07b8
ron erlih <ronerlih@gmail.com>
2021-03-21 19:34:16 -0400

    updated axios

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.post("/upload", function (req, res) {
 
 	axios
 		.get("https://www.google.com/searchbyimage/upload" + urlParmas, {
-				headers: {"User-Agent": ""}
+				headers: {"User-Agent": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)"}
          })
 		.then((result) => {
 			// Handle result…
