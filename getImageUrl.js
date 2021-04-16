33aa6481f8247c528a3347635b0895911ffcf6c5
ron erlih <ronerlih@gmail.com>
2021-04-16 16:20:56 -0400

    restructure click action

--- getImageUrl.js
+++ getImageUrl.js
@@ -1,16 +1,16 @@
 const puppeteer = require("puppeteer");
 
 module.exports = async function getImageUrl(url) {
-	let resourcesLinks = [];
-	const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+	const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
 	const page = await browser.newPage();
 	await page.goto(url);
 
 	// get cookies from rist call
 	const cookies = await page.cookies();
 
-	// screen shot
+   // screen shot
 	await page.screenshot({ path: "images-screenshot.png", fullPage: true });
+
 
 	// Extract the results from the page.
 	const resultsSelector = 'a[href^="/search?sa=G&hl=en&tbs=simg"]';
@@ -18,57 +18,45 @@ module.exports = async function getImageUrl(url) {
 
 	// set cookies from first call
 	await page.setCookie(...cookies);
-	console.log({ links });
+   console.log({links})
 	await page.goto(links[0], { waitUntil: "networkidle0" });
 
 	const gridResultsSelector = 'div[jsdata*="GRID_STATE"] a';
 
-	// generate click
-	resourcesLinks = await evaluateSelector(gridResultsSelector, "click");
+   // get thumbLink
+   thunbLinks = await page.$$(gridResultsSelector)
+   thunbLinks = thunbLinks.filter(anchor => anchor.__jsaction)
+   // generate click
+	thunbLinks.map ( link => evaluateClick(link)}
 
-	console.log("resources links after evaluate:", resourcesLinks);
-	// const imgResultsSelector = 'a[href*="imgres"]';
+	const imgResultsSelector = 'a[href*="imgres"]';
 
-	// const imgLink = await evaluateSelector(imgResultsSelector, "href")
+	const imgLink = await evaluateSelector(imgResultsSelector, "href")
 
-	// screen shot
+   // screen shot
 	// await page.screenshot({ path: "images-screenshot.png", fullPage: true });
 
-	resourcesLinks = resourcesLinks.map((link) => new URLSearchParams(link.toString()).get("https://www.google.com/imgres?imgurl"));
-	page.close();
-	return resourcesLinks;
+	const urlParams = new URLSearchParams(imgLink.toString());
+   page.close();
+   return urlParams.get("https://www.google.com/imgres?imgurl")
+	
 
-	async function evaluateSelector(selector, tag) {
-		const linksArray = [];
 
-		return new Promise(async (resolve, reject) => {
-		var args = [selector, tag, resolve];
-
-			await page.evaluate((args) => {
-				const anchors = Array.from(document.querySelectorAll(args[0]));
-				if (args[1] === "click") {
-					anchors
-						.filter((anchor) => anchor.__jsaction)
-						.map(async (anchor) => {
-							anchor.click();
-							const imgResultsSelector = 'a[href*="imgres"]';
-
-							console.log({ args });
-							const links = await evaluateSelector(imgResultsSelector, "href");
-							console.log("\n\n\n\n\n\n 🇨🇦 evaluate selector (links):", links);
-							linksArray.push(links);
-
-							args[2](linksArray);
-						});
-				} else {
-					args[2](
-						anchors.map((anchor) => {
-							const title = anchor.textContent.split("|")[0].trim();
-							return `${anchor[args[1]]}`;
-						})
-					);
-				}
-			}, args);
-		});
+	async function evaluateClick(link) {
+      var arg = link;
+		return await page.evaluate((arg) => {
+         arg.click();
+      }, arg);
 	}
-};
+	async function evaluateSelector(selector, tag) {
+      var args = [selector, tag];
+
+		return await page.evaluate((args) => {
+			const anchors = Array.from(document.querySelectorAll(args[0]));
+				return anchors.map((anchor) => {
+					const title = anchor.textContent.split("|")[0].trim();
+					return `${anchor[args[1]]}`;
+				});
+		}, args);
+	}
+}
