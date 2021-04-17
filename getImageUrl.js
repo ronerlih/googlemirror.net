9418db4bfa1fe87d48c1cdda00984d48e13b1fbb
ron erlih <ronerlih@gmail.com>
2021-04-16 20:35:39 -0400

    restructure click action

--- getImageUrl.js
+++ getImageUrl.js
@@ -1,16 +1,15 @@
 const puppeteer = require("puppeteer");
 
 module.exports = async function getImageUrl(url) {
-	const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+	const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
 	const page = await browser.newPage();
 	await page.goto(url);
 
 	// get cookies from rist call
 	const cookies = await page.cookies();
 
-   // screen shot
-	await page.screenshot({ path: "images-screenshot.png", fullPage: true });
-
+	// screen shot
+	// await page.screenshot({ path: "images-screenshot.png", fullPage: true });
 
 	// Extract the results from the page.
 	const resultsSelector = 'a[href^="/search?sa=G&hl=en&tbs=simg"]';
@@ -22,53 +21,48 @@ module.exports = async function getImageUrl(url) {
 
 	const gridResultsSelector = 'div[jsdata*="GRID_STATE"] a';
 
-   // get thumbLink
-   let thumbLinks = await page.$$(gridResultsSelector)
+	// get thumbLink
+	let thumbLinks = await page.$$(gridResultsSelector);
 
-   thumbLinks = await thumbLinks.filter(async (anchor, i) => {
-     if(i === 2) {
-        const jsVal =  await anchor.jsonValue();
-      console.log('ancohr jsonValue:', jsVal)
-     }
-      return "link"
-   })
+	thumbLinks = await thumbLinks.filter(async (anchor, i) => {
+		const jsVal = await anchor.jsonValue();
+		return jsVal.__jsaction;
+	});
 
-   console.log({thumbLinks})
-   let imagesLinks = [];
-   // generate click
-	thumbLinks.map ( async link => {
-      await evaluateClick(link);
-      const imgResultsSelector = 'a[href*="imgres"]';
-      const imgLinks = await evaluateSelector(imgResultsSelector, "href")
-      console.log({imgLinks})
-      imagesLinks = imagesLinks.concat(imgLinks)
-   })
+	// console.log({ thumbLinks });
+	let imagesLinks = [];
+	// generate click
+	thumbLinks.map(async (link) => {
+		await evaluateClick(link);
+		const imgResultsSelector = 'a[href*="imgres"]';
+		const imgLinks = await evaluateSelector(imgResultsSelector, "href");
+      imgLinks.map (link => new URLSearchParams(link.toString()));
+		imagesLinks = imagesLinks.concat(imgLinks);
+	});
 
-	console.log({imagesLinks})
-   // screen shot
+	console.log({ imagesLinks });
+	// screen shot
 	// await page.screenshot({ path: "images-screenshot.png", fullPage: true });
 
-	// const urlParams = new URLSearchParams(imgLink.toString());
-   // page.close();
-   // return urlParams.get("https://www.google.com/imgres?imgurl")
-	return ["https://gravatar.com/avatar/4977c2c87b44a2ccec8e02dc7c1cf643?s=96&d=https://www.herokucdn.com/images/ninja-avatar-96x96.png"]
-
+	page.close();
+	// return urlParams.get("https://www.google.com/imgres?imgurl")
+	return imagesLinks;
 
 	async function evaluateClick(link) {
-      var arg = link;
+		var arg = link;
 		return await page.evaluate((arg) => {
-         arg.click();
-      }, arg);
+			arg.click();
+		}, arg);
 	}
 	async function evaluateSelector(selector, tag) {
-      var args = [selector, tag];
+		var args = [selector, tag];
 
 		return await page.evaluate((args) => {
 			const anchors = Array.from(document.querySelectorAll(args[0]));
-				return anchors.map((anchor) => {
-					const title = anchor.textContent.split("|")[0].trim();
-					return `${anchor[args[1]]}`;
-				});
+			return anchors.map((anchor) => {
+				const title = anchor.textContent.split("|")[0].trim();
+				return `${anchor[args[1]]}`;
+			});
 		}, args);
 	}
-}
+};
