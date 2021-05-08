e728030ec3815c4d79f78b0bf7fff6b9bdd9b2bd
ron erlih <ronerlih@gmail.com>
2021-05-08 18:32:37 -0400

    video fade out

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,6 @@
 var img = document.querySelector("img");
 var canvas = document.querySelector("canvas");
 var video = document.querySelector("video");
-var videoEl = $("video");
 var uiFrame = document.getElementById("uiFrame");
 var imgContainer =$("#img-container");
 var themeToggle = $("#theme-toggle");
@@ -172,10 +171,6 @@ function imageFrontEndTransition() {
 }
 
 function startGoogleMirrorTimers() {
-   setTimeout(function () {
-      setElemetsSizes();
-   }, 1);
-
    if (firstFadeInFlag == true) {
       intervalReset = setInterval(fadeCameraIn, 10);
       //catch all devices video initiation timing
@@ -184,7 +179,9 @@ function startGoogleMirrorTimers() {
    } else {
       intervalReset = setInterval(fadeCameraIn, 15);
    }
-   
+   setTimeout(function () {
+      setElemetsSizes();
+   }, 1);
    console.log("googleMirror timers restarted.. get ready.. ");
 }
 function fadeCameraIn() {
@@ -287,24 +284,22 @@ function blobToFile(theBlob, fileName) {
    return theBlob;
 }
 function setElemetsSizes() {
+   themeToggle.show();
+   themeToggle.on('click', toggleFullScreen)
+
+   var videoEl = $("video");
    videoElementWidth = videoEl.width();
+   // videoEl.width(videoElementWidth)
    var videoElementHeight = videoEl.height();
    $("#img").width(videoElementWidth);
    $("#img").height(videoElementHeight);
-
-   if (firstFadeInFlag) {
-
-   themeToggle.show();
-   themeToggle.on('click', toggleFullScreen);
-
    imgContainer.width(videoElementWidth);
+   // $("#infoDiv").width(videoElementWidth)
+   // $("#infoDiv").css("margin-top", videoElementHeight + 80);
+   //                $('#loadingSpinner').css('top', ( videoElementHeight + 100 ) * 0.5 );
    $("#infoDiv, #studioCredit").show();
    var docHeight = $("#infoDiv").height() + $("#infoDiv").offset();
    $("#studioCredit").css("top", docHeight);
-}
-
-   
-   
 }
 function toggleFullScreen() {
    this.style.background = "black"
